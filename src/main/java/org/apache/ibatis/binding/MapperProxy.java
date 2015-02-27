1f4d8c8327441210a46a7fa96ecf00186e12f914
zhengzhihust <1424798497@qq.com>
2015-02-27 10:18:15 +0800

    bb

--- src/main/java/org/apache/ibatis/binding/MapperProxy.java
+++ src/main/java/org/apache/ibatis/binding/MapperProxy.java
@@ -29,37 +29,40 @@ import org.apache.ibatis.session.SqlSession;
  */
 public class MapperProxy<T> implements InvocationHandler, Serializable {
 
-  private static final long serialVersionUID = -6424540398559729838L;
-  private final SqlSession sqlSession;
-  private final Class<T> mapperInterface;
-  private final Map<Method, MapperMethod> methodCache;
+	private static final long serialVersionUID = -6424540398559729838L;
+	private final SqlSession sqlSession;
+	private final Class<T> mapperInterface;
+	private final Map<Method, MapperMethod> methodCache;
 
-  public MapperProxy(SqlSession sqlSession, Class<T> mapperInterface, Map<Method, MapperMethod> methodCache) {
-    this.sqlSession = sqlSession;
-    this.mapperInterface = mapperInterface;
-    this.methodCache = methodCache;
-  }
+	public MapperProxy(SqlSession sqlSession, Class<T> mapperInterface,
+			Map<Method, MapperMethod> methodCache) {
+		this.sqlSession = sqlSession;
+		this.mapperInterface = mapperInterface;
+		this.methodCache = methodCache;
+	}
 
-  @Override
-  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-    if (Object.class.equals(method.getDeclaringClass())) {
-      try {
-        return method.invoke(this, args);
-      } catch (Throwable t) {
-        throw ExceptionUtil.unwrapThrowable(t);
-      }
-    }
-    final MapperMethod mapperMethod = cachedMapperMethod(method);
-    return mapperMethod.execute(sqlSession, args);
-  }
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args)
+			throws Throwable {
+		if (Object.class.equals(method.getDeclaringClass())) {
+			try {
+				return method.invoke(this, args);
+			} catch (Throwable t) {
+				throw ExceptionUtil.unwrapThrowable(t);
+			}
+		}
+		final MapperMethod mapperMethod = cachedMapperMethod(method);
+		return mapperMethod.execute(sqlSession, args);
+	}
 
-  private MapperMethod cachedMapperMethod(Method method) {
-    MapperMethod mapperMethod = methodCache.get(method);
-    if (mapperMethod == null) {
-      mapperMethod = new MapperMethod(mapperInterface, method, sqlSession.getConfiguration());
-      methodCache.put(method, mapperMethod);
-    }
-    return mapperMethod;
-  }
+	private MapperMethod cachedMapperMethod(Method method) {
+		MapperMethod mapperMethod = methodCache.get(method);
+		if (mapperMethod == null) {
+			mapperMethod = new MapperMethod(mapperInterface, method,
+					sqlSession.getConfiguration());
+			methodCache.put(method, mapperMethod);
+		}
+		return mapperMethod;
+	}
 
 }
