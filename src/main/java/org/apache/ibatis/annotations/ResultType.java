be1e05e45396e1824c4d2926dfdf173c3053833d
zhengzhihust <1424798497@qq.com>
2015-02-27 10:01:12 +0800

    commit

--- src/main/java/org/apache/ibatis/annotations/ResultType.java
+++ src/main/java/org/apache/ibatis/annotations/ResultType.java
@@ -21,10 +21,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * This annotation can be used when a @Select method is using a
- * ResultHandler.  Those methods must have void return type, so
- * this annotation can be used to tell MyBatis what kind of object
- * it should build for each row.
+ * This annotation can be used when a @Select method is using a ResultHandler.
+ * Those methods must have void return type, so this annotation can be used to
+ * tell MyBatis what kind of object it should build for each row.
  * 
  * @since 3.2.0
  * @author Jeff Butler
@@ -32,5 +31,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface ResultType {
-  Class<?> value();
+	Class<?> value();
 }
